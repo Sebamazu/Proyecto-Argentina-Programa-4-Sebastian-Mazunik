941964fd63bd4e7ca09c78a2c71373704288aeeb
Sebamazu <mazunikseba@gmail.com>
2023-03-04 23:58:29 -0300

    html , css y js
    Se agrego funciones en html como botones, estilos css, algunas funciones en js

---
+++ js/funcion.js
@@ -0,0 +1,11 @@
+
+document.getElementById('botonexpe').onclick = function(){
+    document.getElementById('laboral').style.display = 'block'
+    document.getElementById('educacion').style.display = 'block'
+}
+document.getElementById('boton-nombre').onmouseover = function(){
+    document.getElementById('contacto').style.display = 'block'
+}
+document.getElementById('boton-nombre').onmouseout = function(){
+    document.getElementById('contacto').style.display = 'none'
+}
