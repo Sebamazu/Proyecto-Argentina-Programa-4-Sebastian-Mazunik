f14e997d9d0f71dcb72c46fe6058b66d3be63696
Sebamazu <mazunikseba@gmail.com>
2023-03-06 15:46:45 -0300

    Cambios esteticos
    Agregar la seccion contactos y subrayar palabras claves

--- js/funcion.js
+++ js/funcion.js
@@ -3,6 +3,7 @@ document.getElementById('botonexpe').onclick = function(){
     document.getElementById('laboral').style.display = 'block'
     document.getElementById('educacion').style.display = 'block'
 }
+
 document.getElementById('boton-nombre').onmouseover = function(){
     document.getElementById('contacto').style.display = 'block'
 }
