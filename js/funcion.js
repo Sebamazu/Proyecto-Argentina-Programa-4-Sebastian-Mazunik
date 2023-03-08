8cadb1158fa86971467371e1de68c9d82bea9968
Sebamazu <mazunikseba@gmail.com>
2023-03-08 00:24:59 -0300

    Navbar
    Se agrego navbar, estilos, favicon  y detalles finales

--- js/funcion.js
+++ js/funcion.js
@@ -10,3 +10,34 @@ document.getElementById('boton-nombre').onmouseover = function(){
 document.getElementById('boton-nombre').onmouseout = function(){
     document.getElementById('contacto').style.display = 'none'
 }
+
+document.getElementById('banerr2').onmouseover = function(){
+    document.getElementById('banerr').style.color = 'white'
+}
+document.getElementById('banerr2').onmouseout = function(){
+    document.getElementById('banerr').style.color = 'black'
+}
+document.getElementById('sobre2').onmouseover = function(){
+    document.getElementById('sobre').style.color = 'white'
+}
+document.getElementById('sobre2').onmouseout = function(){
+    document.getElementById('sobre').style.color = 'black'
+}
+document.getElementById('expe2').onmouseover = function(){
+    document.getElementById('expe').style.color = 'white'
+}
+document.getElementById('expe2').onmouseout = function(){
+    document.getElementById('expe').style.color = 'black'
+}
+document.getElementById('skil2').onmouseover = function(){
+    document.getElementById('skil').style.color = 'white'
+}
+document.getElementById('skil2').onmouseout = function(){
+    document.getElementById('skil').style.color = 'black'
+}
+document.getElementById('proye2').onmouseover = function(){
+    document.getElementById('proye').style.color = 'white'
+}
+document.getElementById('proye2').onmouseout = function(){
+    document.getElementById('proye').style.color = 'black'
+}
